a166b3cc6ef15c8113e212e4103f1460b255e3dc
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2024-02-01 09:40:37 -0300

    commit para devs

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,16 @@
 import { useContext } from 'react';
 
-import { UserContext } from '../../contexts/UserContext';
+import { AuthContext } from '../../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 
 function Home() {
-  const { nome } = useContext(UserContext);
+  const { email } = useContext(AuthContext);
 
   return (
     <div className='flex justify-center items-center'>
       <div>
         <h2 className="text-slate-900 text-5xl  my-4">Logar</h2>
-        <h2 className="text-slate-900 text-4xl ">Ola user : {nome}</h2>
+        <h2 className="text-slate-900 text-4xl ">Ola user : {email.nome}</h2>
         <Link to="/login" className="my-4 rounded bg-indigo-400
          hover:bg-indigo-900 text-white w-1/2 py-2 flex justify-center">
           Voltar 
