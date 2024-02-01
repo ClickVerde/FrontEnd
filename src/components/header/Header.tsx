6fb353449305e129ab30bfb7c4fa16191011df52
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2024-02-01 17:26:38 -0300

    Header, Login e Footer adicionados

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,12 @@
 import { useContext } from 'react'
+import SearchIcon from '../../assets/icons/SearchIcon.svg'
+import Seta from '../../assets/icons/seta.svg'
+import Logo from '../../assets/Logo.svg';
 import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-
-
+import User from '../../assets/icons/usericon.svg';
+import Cart from '../../assets/icons/carticon.svg';
+import '../../index.css';
 
 function Navbar() {
   let navigate = useNavigate()
@@ -20,20 +24,55 @@ function Navbar() {
 
   return (
     <>
-      <div className='w-full bg-[#21882a] text-white flex justify-center py-4'>
-        <div className="container flex justify-between text-lg">
-          <Link to='/home' className='text-2xl font-bold uppercase'>Clique</Link>
 
-          <div className='flex gap-4'>
-            <div className='font-bold cursor-pointer text-[#fde845] hover:text-[#2d2c24]'>Postagens</div>
-            <Link to='/temas' className='font-bold cursor-pointer hover:text-[#242b2d]'>Temas</Link>
-            <Link to='/cadastroTema' className='font-bold cursor-pointer hover:text-[#242b2d]'>Cadastrar tema</Link>
-            <div className='font-bold cursor-pointer hover:text-[#242b2d]'>Perfil</div>
-            <Link to='' onClick={logout} className='font-bold cursor-pointer hover:text-[#242b2d]'>Sair</Link>
+      <header className='w-full bg-white text-white flex justify-between align-middle py-[1rem] px-[4rem] '>
+        <div>
+          <img src={Logo} className='xl:w-28 sm:w-25'></img>
+        </div>
+
+        <section className='flex justify-between gap-6'>
+          <div className='text-darkMossGreen p-4 grid gap-10 grid-flow-col items-center font-bold text-[20px] ]'>
+            <a >Home</a>
+            <a >Produtos</a>
+            <a className='flex' >
+              <p>Categorias</p>
+              <img src={Seta} className='px-1'></img>
+            </a>
+            <a >Sobre</a>
+          </div>
+
+          <div className='div-focus flex p-[5px] rounded-full self-center bg-seasalt border border-emerald input-pesquisa'>
+            <input
+              type="text"
+              placeholder="Pesquisar"
+              name='descricao'
+              className="w-[15vw] h-[2vw] border-none rounded-full p-2 self-center input-pesquisa"
+            />
+            <button>
+              <img className='w-10' src={SearchIcon} alt="" />
+            </button>
+
+
 
           </div>
-        </div>
-      </div>
+
+          <div className='gap-3 justify-around flex items-center p-3'>
+
+            <div className='flex rounded-[12rem]  items-center border border-emerald p-[5px]'>
+              <img src={User} className='w-10'></img>
+              <p className='headerBtn text-darkMossGreen  ps-2 pe-1'>Perfil</p>
+              <img src={Seta} className='pe-1'></img>
+            </div>
+
+
+            <div className='flex rounded-[12rem] items-center border-emerald border p-[5px]'>
+              <img src={Cart} className='w-10'></img>
+              <p className='headerBtn text-darkMossGreen p-2'>Carrinho</p>
+            </div>
+
+          </div>
+        </section>
+      </header>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ export const content = [
   "./src/**/*.{js,ts,jsx,tsx}",
 ];
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      'emerald': '#7BD389', // Escrever (attribute-)emerald para usar
+      'darkMossGreen': '#3E5622',
+      'sunglow': '#FFD242',
+      'davysGray': '#525C60',
+      'seasalt': '#FAFAFA',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'], // Escrever font-roboto para usar
+      openSans: ['Open Sans', 'sans-serif'] // Escrever font-openSans para usar
+    },
+  },
 };
 export const plugins = [];
