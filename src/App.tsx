86c37b5e411210752f4496326aaac374b1e7cefd
Henrique M. N. <henriquemancininevado@gmail.com>
2024-02-01 11:44:31 -0300

    Mudança da rota padrão para Home ao invés de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,50 @@
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/header/Header';
-import Footer from './components/footer/Footer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/login/Login';
-import Cadastro from './pages/cadastro/Cadastro';
-import Home from './pages/home/Home';
-import { AuthProvider } from './contexts/AuthContext';
-import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria';
-import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
-import ListaCategoria from './components/categorias/listaCategoria/ListaCategoria';
-
+import Navbar from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+import { AuthProvider } from "./contexts/AuthContext";
+import DeletarCategoria from "./components/categorias/deletarCategoria/DeletarCategoria";
+import FormularioCategoria from "./components/categorias/formularioCategoria/FormularioCategoria";
+import ListaCategoria from "./components/categorias/listaCategoria/ListaCategoria";
 
 function App() {
-  return (
-    <>
-    <AuthProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/categorias/all" element={<ListaCategoria />} />
-              <Route 
-                path="/cadastroCategoria" 
-                element={<FormularioCategoria />} 
-              />
-              <Route 
-                path="/editarCategoria/:id" 
-                element={<FormularioCategoria />} 
-              />
-              <Route 
-                path="/deletarCategoria/:id" 
-                element={<DeletarCategoria />} 
-              /> 
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-        </AuthProvider>
-    </>
-  );
+	return (
+		<>
+			<AuthProvider>
+				<BrowserRouter>
+					<Navbar />
+					<div className="min-h-[80vh]">
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/cadastro" element={<Cadastro />} />
+							<Route path="/home" element={<Home />} />
+							<Route
+								path="/categorias/all"
+								element={<ListaCategoria />}
+							/>
+							<Route
+								path="/cadastroCategoria"
+								element={<FormularioCategoria />}
+							/>
+							<Route
+								path="/editarCategoria/:id"
+								element={<FormularioCategoria />}
+							/>
+							<Route
+								path="/deletarCategoria/:id"
+								element={<DeletarCategoria />}
+							/>
+						</Routes>
+					</div>
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>
+		</>
+	);
 }
 export default App;
