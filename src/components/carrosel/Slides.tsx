be246b3ae9a744551c6a7ea375fa619d7b450f55
WendyEugenia <wendy.t.n@hotmail.com>
2024-02-06 14:28:53 -0300

    add homepage

---
+++ src/components/carrosel/Slides.tsx
@@ -0,0 +1,52 @@
+function Slides() {
+    return (
+        <div className="bg-darkMossGreen flex justify-center mb-5">
+            <div className="container flex flex-col items-center justify-center text-white">
+                <div className="mt-4">
+                    <h2 className="font-Roboto text-white">Conheça nossas categorias</h2>
+                </div>
+                <div className="mb-4"></div>
+                <div className="flex justify-center gap-5 mb-9">
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow  text-white" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-darkMossGreen bold">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-black">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-black">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-black">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-black">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                    <div className="bg-white p-4 rounded-lg text-center flex-grow" style={{ width: "200px", height: "200px", borderRadius: "30px" }}>
+                        <p className="text-black">Brinquedos</p>
+                        <div className="mt-5">
+                            <img src="" alt="Imagem Brinquedos" className="w-16 h-16" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Slides;
